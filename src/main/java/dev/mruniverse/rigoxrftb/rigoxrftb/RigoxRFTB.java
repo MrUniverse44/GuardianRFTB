1ce61d9ea88604c9f51aac9f5a7810fb44b1eb96
MrUniverse44 <justinalxhd@gmail.com>
2021-01-17 14:20:47 -0500

    Initial commit

---
+++ src/main/java/dev/mruniverse/rigoxrftb/rigoxrftb/RigoxRFTB.java
@@ -0,0 +1,17 @@
+package dev.mruniverse.rigoxrftb.rigoxrftb;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RigoxRFTB extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
