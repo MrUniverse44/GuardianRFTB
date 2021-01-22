e4b1188f89cda2546bf3c8063198d099d5fb0893
MrUniverse44 <justinalxhd@gmail.com>
2021-01-21 21:59:20 -0500

    Finished Support for 1.8.x to 1.16.x

--- Core/src/main/java/dev/mruniverse/rigoxrftb/core/enums/NMSenum.java
+++ Core/src/main/java/dev/mruniverse/rigoxrftb/core/enums/NMSenum.java
@@ -1,9 +1,9 @@
 package dev.mruniverse.rigoxrftb.core.enums;
 
 import dev.mruniverse.rigoxrftb.core.utils.versionVerificator;
-
+@SuppressWarnings("unused")
 public enum NMSenum {
-    v1_8_R2, v1_8_R3, v1_9_R1, v1_9_R2, v1_10_R1, v1_11_R1, v1_12_R1, v1_13_R1, v1_13_R2, v1_14_R1, v1_15_R1, v1_16_R1, v1_16_R2, v1_16_R3;
+    v1_8_R1,v1_8_R2, v1_8_R3, v1_9_R1, v1_9_R2, v1_10_R1, v1_11_R1, v1_12_R1, v1_13_R1, v1_13_R2, v1_14_R1, v1_15_R1, v1_16_R1, v1_16_R2, v1_16_R3;
 
     private static final NMSenum CURRENT_VERSION;
 
