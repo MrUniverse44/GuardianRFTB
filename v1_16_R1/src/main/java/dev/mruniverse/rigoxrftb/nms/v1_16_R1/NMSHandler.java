f828a34f3c1298287eb9fc06c45a51ace2b18d14
MrUniverse44 <justinalxhd@gmail.com>
2021-01-21 21:03:50 -0500

    Re-Added 1_16_R1 Support

---
+++ v1_16_R1/src/main/java/dev/mruniverse/rigoxrftb/nms/v1_16_R1/NMSHandler.java
@@ -0,0 +1,44 @@
+package dev.mruniverse.rigoxrftb.nms.v1_16_R1;
+
+import dev.mruniverse.rigoxrftb.core.nms.NMS;
+import net.md_5.bungee.api.ChatColor;
+import net.minecraft.server.v1_16_R1.*;
+import org.bukkit.craftbukkit.v1_16_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class NMSHandler implements NMS {
+    public void sendTitle(Player player, int fadeIn, int stay, int fadeOut, String title, String subtitle) {
+        PlayerConnection pConn = (((CraftPlayer)player).getHandle()).playerConnection;
+        PacketPlayOutTitle pTitleInfo = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TIMES, null, fadeIn, stay, fadeOut);
+        pConn.sendPacket(pTitleInfo);
+        if (subtitle != null) {
+            subtitle = subtitle.replaceAll("%player%", player.getName());
+            subtitle = ChatColor.translateAlternateColorCodes('&', subtitle);
+            IChatMutableComponent iChatMutableComponent = IChatBaseComponent.ChatSerializer.a("{\"text\": \"" + subtitle + "\"}");
+            PacketPlayOutTitle pSubtitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.SUBTITLE, iChatMutableComponent);
+            pConn.sendPacket(pSubtitle);
+        }
+        if (title != null) {
+            title = title.replaceAll("%player%", player.getName());
+            title = ChatColor.translateAlternateColorCodes('&', title);
+            IChatMutableComponent iChatMutableComponent = IChatBaseComponent.ChatSerializer.a("{\"text\": \"" + title + "\"}");
+            PacketPlayOutTitle pTitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TITLE, iChatMutableComponent);
+            pConn.sendPacket(pTitle);
+        }
+    }
+
+    public void sendActionBar(Player player, String msg) {
+        String toBC = ChatColor.translateAlternateColorCodes('&', msg);
+        IChatMutableComponent iChatMutableComponent = IChatBaseComponent.ChatSerializer.a("{\"text\": \"" + toBC + "\"}");
+        PacketPlayOutTitle bar = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.ACTIONBAR, iChatMutableComponent);
+        (((CraftPlayer)player).getHandle()).playerConnection.sendPacket(bar);
+    }
+
+    public void sendBossBar(Player player, String message) {
+
+    }
+
+    public void sendBossBar(Player player, String message, Integer percentage) {
+
+    }
+}
