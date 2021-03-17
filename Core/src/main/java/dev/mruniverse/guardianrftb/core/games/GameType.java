849957600197e8a181f713e51d1591dd7f0b589e
MrUniverse44 <oficial.mruniverse@gmail.com>
2021-03-17 09:45:19 -0500

    Removed Comments

--- Core/src/main/java/dev/mruniverse/guardianrftb/core/games/GameType.java
+++ Core/src/main/java/dev/mruniverse/guardianrftb/core/games/GameType.java
@@ -7,11 +7,10 @@ import org.bukkit.configuration.file.FileConfiguration;
 public enum GameType {
     DOUBLE_BEAST,
     INFECTED,
-    KILLER, //NEW MODE
-    ISLAND_OF_THE_BEAST, // NEW MODE
-    ISLAND_OF_THE_BEAST_DOUBLE_BEAST, //NEW MODE
-    ISLAND_OF_THE_BEAST_KILLER, //NEW MODE
-    //HISTORY_MODE, //NEW MODE
+    KILLER,
+    ISLAND_OF_THE_BEAST,
+    ISLAND_OF_THE_BEAST_DOUBLE_BEAST,
+    ISLAND_OF_THE_BEAST_KILLER,
     CLASSIC;
 
     public String getType() {
@@ -29,8 +28,6 @@ public enum GameType {
                 return file.getString("gameType.DOUBLE_BEAST");
             case INFECTED:
                 return file.getString("gameType.INFECTED");
-            //case HISTORY_MODE:
-            //    return file.getString("gameType.HISTORY_MODE");
             case CLASSIC:
             default:
                 return file.getString("gameType.CLASSIC");
