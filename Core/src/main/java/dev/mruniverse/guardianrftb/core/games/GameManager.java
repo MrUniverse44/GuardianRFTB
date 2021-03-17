1fb5d2e254c458999e1a09f62da831d7c2f52c38
MrUniverse44 <oficial.mruniverse@gmail.com>
2021-03-17 13:33:09 -0500

    Moved GameMenus

--- Core/src/main/java/dev/mruniverse/guardianrftb/core/games/GameManager.java
+++ Core/src/main/java/dev/mruniverse/guardianrftb/core/games/GameManager.java
@@ -46,6 +46,13 @@ public class GameManager {
 
     public void loadGames() {
         try {
+            gameMenu.put(GameType.CLASSIC,new GameMenu(plugin,GameType.CLASSIC));
+            gameMenu.put(GameType.DOUBLE_BEAST,new GameMenu(plugin,GameType.DOUBLE_BEAST));
+            gameMenu.put(GameType.INFECTED,new GameMenu(plugin,GameType.INFECTED));
+            gameMenu.put(GameType.KILLER,new GameMenu(plugin,GameType.KILLER));
+            gameMenu.put(GameType.ISLAND_OF_THE_BEAST,new GameMenu(plugin,GameType.ISLAND_OF_THE_BEAST));
+            gameMenu.put(GameType.ISLAND_OF_THE_BEAST_DOUBLE_BEAST,new GameMenu(plugin,GameType.ISLAND_OF_THE_BEAST_DOUBLE_BEAST));
+            gameMenu.put(GameType.ISLAND_OF_THE_BEAST_KILLER,new GameMenu(plugin,GameType.ISLAND_OF_THE_BEAST_KILLER));
             if(plugin.getStorage().getControl(GuardianFiles.GAMES).contains("games")) {
                 for (String gameName : Objects.requireNonNull(plugin.getStorage().getControl(GuardianFiles.GAMES).getConfigurationSection("games")).getKeys(false)) {
                     if(plugin.getStorage().getControl(GuardianFiles.GAMES).getBoolean("games." + gameName + ".enabled")) {
@@ -61,13 +68,6 @@ public class GameManager {
                 plugin.getLogs().info("You don't have games created yet.");
             }
             plugin.getServer().getScheduler().runTaskTimerAsynchronously(plugin,new GameRunnable(plugin),0L,20L);
-            gameMenu.put(GameType.CLASSIC,new GameMenu(plugin,GameType.CLASSIC));
-            gameMenu.put(GameType.DOUBLE_BEAST,new GameMenu(plugin,GameType.DOUBLE_BEAST));
-            gameMenu.put(GameType.INFECTED,new GameMenu(plugin,GameType.INFECTED));
-            gameMenu.put(GameType.KILLER,new GameMenu(plugin,GameType.KILLER));
-            gameMenu.put(GameType.ISLAND_OF_THE_BEAST,new GameMenu(plugin,GameType.ISLAND_OF_THE_BEAST));
-            gameMenu.put(GameType.ISLAND_OF_THE_BEAST_DOUBLE_BEAST,new GameMenu(plugin,GameType.ISLAND_OF_THE_BEAST_DOUBLE_BEAST));
-            gameMenu.put(GameType.ISLAND_OF_THE_BEAST_KILLER,new GameMenu(plugin,GameType.ISLAND_OF_THE_BEAST_KILLER));
             //gameMenu = new GameMenu(plugin);
             loadGameWorlds();
         }catch (Throwable throwable) {
